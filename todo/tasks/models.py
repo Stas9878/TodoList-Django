32f7cd4c5debc0385d8caf8b364f91d426dca873
Popov Stas <boss.staspopov@mail.ru>
2024-05-26 14:59:58 +0400

    create model task and subtask

--- todo/tasks/models.py
+++ todo/tasks/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.CharField(max_length=250)
+    assigned_to = models.ManyToManyField(get_user_model())
+    creation_date = models.DateField(auto_now=True)
+    due_date = models.DateField(blank=True, null=True)
+    completed = models.BooleanField(default=False)
+
+
+class SubTask(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.CharField(max_length=250)
+    assigned_to = models.ManyToManyField(get_user_model())
+    creation_date = models.DateField(auto_now=True)
+    due_date = models.DateField(blank=True, null=True)
+    completed = models.BooleanField(default=False)
+    parent_task = models.ForeignKey(Task, on_delete=models.CASCADE)
